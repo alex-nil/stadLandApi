2760a0a484f45ed7a5ec62cdd0ae2e1c835e1ea1
Alexander Nilsson <nial19qa@student.ju.se>
2019-10-28 14:20:01 +0100

    API CORS

--- routes/land.js
+++ routes/land.js
@@ -5,6 +5,8 @@ var urlencodedParser = bodyParser.urlencoded({ extended: true});
 var router = express.Router();
 /* GET users listing. */
 router.get('/', function(req, res, next)  {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   fs.readFile("./land.json", (err,data) => {
     if (err) throw err;
     var land = JSON.parse(data);
--- routes/stad.js
+++ routes/stad.js
@@ -5,6 +5,8 @@ var urlencodedParser = bodyParser.urlencoded({ extended: true});
 var router = express.Router();
 /* GET users listing. */
 router.get('/', function(req, res, next)  {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   fs.readFile("./stad.json", (err,data) => {
     if (err) throw err;
     var stad = JSON.parse(data);
